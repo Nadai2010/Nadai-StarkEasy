a726d3b767129b463cf3f8241bf82da05da02abc
Nadai2010 <nadai2010@proton.me>
2023-09-02 11:26:59 +0100

    Update HDS 11

--- src/components/HDS.tsx
+++ src/components/HDS.tsx
@@ -153,6 +153,7 @@ const secretWords: SecretWords = {
   jugar: "0x02269df2f4cae317c90fdcb1a8fa8fbe21744ac9e197721ecaa3baf14c1b3571",
   planeta: "0x018561e9475a9248f0580e3274fb8a027b33850dbd2e53f2d6acb9c14fcd0599",
   comunidad: "0x02adcd276ac03ca4b056a679fc312e0fa8ff9bd4adbfb8e6398782af4932d3a2",
+  fruta: "0x00aa901204eb07aa024f7ec23da133f53e6609c32c1a7238fdbba7a332a494fa",
 };
 
 const HDS = () => {
